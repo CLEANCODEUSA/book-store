7bc7db8daa54f2b77984329be8ade5d6528d5f54
CLEANCODE <help@simplecssjs.com>
2025-12-24 11:16:25 -0500

    Post request using Axios

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Button, FlatList, Alert } from 'react-native';
 import axios from 'axios'
 import { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
 
@@ -38,13 +39,31 @@ export default function App() {
     }
   }
 
+  const body = {
+    name_of_author: "Last Book",
+    cover: "https://images.unsplash.com/photo-1580008006944-bb3b7e890d5d",
+    price_of_book: "500",
+    email_of_seller: "emma@gmail.com"
+  }
+
+  const createBook = async () => {
+    try {
+      const response = await axios.post(endpointURL, body)
+      Alert.alert("Book has been created.")
+      getListOfBooks();
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
       <View>
         <Button title="Get List of Books" onPress={getListOfBooks} />
         <Button title="Get Book by ID" onPress={getBookByID} />
         <Button title="Delete Book by ID" onPress={deleteBookByID} />
+        <Button title="Create Book" onPress={createBook} />
         <FlatList
           data={bookList}
           keyExtractor={(item) => item.id.toString()}
@@ -62,7 +81,7 @@ export default function App() {
           }
         />
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -71,6 +90,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
