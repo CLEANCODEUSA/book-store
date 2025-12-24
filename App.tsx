852002be8d8a1cf50230dd79c9834cb5691371bd
CLEANCODE <help@simplecssjs.com>
2025-12-24 09:32:47 -0500

    Add Axios

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import axios from 'axios'
 
 export default function App() {
+
+  const endpointURL = "https://694b54ac26e870772067d39d.mockapi.io/books"
+
+  const getListOfBooks = async () => {
+    const response = await axios.get(endpointURL);
+    console.log(JSON.stringify(response.data, null, 3))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View>
+        <Button title="Get List of Books" onPress={getListOfBooks} />
+      </View>
     </View>
   );
 }
